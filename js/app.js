c875c6baf1793c05c7f11d16912e33b7eca6b962
vikas singh <pokhriyalvikas940@gmail.com>
2024-03-11 18:33:14 +0530

    deep

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let button1 = document.getElementById('btn-1');
 let button2 = document.getElementById('btn-2');
 let copycss = document.getElementById('copycode');
+let copybtn = document.getElementById('copy-btn');
+
 
 
 
@@ -13,7 +15,7 @@ const hexValue = () => {
     return colors;
 };
 
-let rgb1 = '#C09173 ';
+let rgb1 = '#C09173';
 let rgb2 = '#86f8e8';
 
 const handleButton1 = () => {
@@ -21,8 +23,8 @@ const handleButton1 = () => {
     button1.innerText = rgb1;
     document.body.style.backgroundImage = `linear-gradient(to right , ${rgb1} , ${rgb2})`;
     copycss.innerHTML = ` background-image: linear-gradient(to right, ${rgb1} , ${rgb2});`;
-
 }
+
 
 const handleButton2 = () => {
     rgb2 = hexValue();
@@ -32,8 +34,12 @@ const handleButton2 = () => {
 }
 
 
-
-
-
 button1.addEventListener('click', handleButton1);
 button2.addEventListener('click', handleButton2);
+
+copybtn.addEventListener('click', () => {
+    navigator.clipboard.writeText(copycss.innerText);
+    alert('copy to clipboard');
+})
+
+
